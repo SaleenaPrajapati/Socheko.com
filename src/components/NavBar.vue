d94a6feeb51ddb6457c6d7c9257c2e973db70f82
SalinaPrajapati <saleenaprajapati1@gmai.com>
2022-05-16 23:39:51 +0545

    first push

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="tabs is-right">
+            <ul>
+                <li><a>Sell on Socheko.com</a></li>
+                <li><a>Mahila Udhyami</a></li>
+                <li><a>SignUp</a></li>
+                <li><a>Login</a></li>
+                <li> <a>
+        <span>Right</span>
+        <span class="icon"><i class="fas fa-angle-right" aria-hidden="true"></i></span>
+      </a></li>
+            </ul>
+        </div>
+    </div>
+</template>
